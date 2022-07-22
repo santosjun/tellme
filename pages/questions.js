bb88067bf674ae87bfcf6039859ed4e02072db74
Jun Santos <42742260+santosjun@users.noreply.github.com>
2022-07-22 17:05:49 +0200

    Initial round

---
+++ pages/questions.js
@@ -0,0 +1,22 @@
+export const questions = [
+  "What is one thing you will never do again?",
+  "What happened the last time you cried?",
+  "What happened the time in your life you were the most nervous?",
+  "What's your worst habit?",
+  "What superpower would you have for one day?",
+  "Where would you live if you could live anywhere in the world?",
+  "What is your most bizarre pet peeve?",
+  "Who knows you best?",
+  "What after-school activities did you do in high school?",
+  "What 'most likely to' superlative would you be most honoured to receive?",
+  "What's the last book you really loved?",
+  "If you could go back in time, what's one piece of advice you could give your younger self?",
+  "What one thing would you be disappointed of if you never got to experience it?",
+  "Apologise or ask permission?",
+  "If you knew you would die in one week, what would you do?",
+  "What's most-listened-to song?",
+  "If you could have been a child prodigy, what would you have wante to be skilled at?",
+  "Which celebrity would you trade lives with?",
+  "If earth could only have one condiment, what would you pick to keep around?",
+  "What's the ideal number of people to have over on a Friday night?"
+]
